29830e70c69db269106ba0398ddecdbf7992c42a
Mathéo Chaumet-Venier <matheo.chaumet.venier@gmail.com>
2023-11-06 15:52:12 +0100

    renaming View

---
+++ src/View/template.php
@@ -0,0 +1,24 @@
+<?php
+
+class Template
+{
+
+    public static function render(array $datas = [])
+    {
+?>
+        <!DOCTYPE html>
+        <html lang="en">
+
+        <head>
+
+            <title>Document</title>
+        </head>
+
+        <body>
+
+        </body>
+
+        </html>
+<?php
+    }
+}
