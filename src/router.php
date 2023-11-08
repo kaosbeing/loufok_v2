0d19ff483d0be5b252502efb3fd16d09732a7544
Mathéo Chaumet-Venier <matheo.chaumet.venier@gmail.com>
2023-11-08 19:35:15 +0100

    router security update

--- src/router.php
+++ src/router.php
@@ -1,6 +1,7 @@
 <?php
 
 session_start();
+date_default_timezone_set('Europe/Paris');
 
 require 'routes.php';
 
@@ -11,6 +12,21 @@ $route = $route === '' ? '/' : $route;
 
 foreach ($routes as $r) {
     if (in_array($route, $r['route']) && $_SERVER['REQUEST_METHOD'] === $r['method']) {
+
+        // Si la route contient le mot "admin", on vérifie les droits
+        // ! Ce code empêche uniquement  les non-admin d'accéder aux routes admin, pas l'inverse
+        if (in_array("admin", explode($r['route'], "/"))) {
+            if (isset($_COOKIE['email']) && isset($_COOKIE['token'])) {
+                $user = AdministrateurModel::getInstance()->findBy(['ad_mail_administrateur' => $_COOKIE['email']])[0];
+
+                if ($_COOKIE['token'] != $user['token']) {
+                    ErrorController::page403();
+                    exit;
+                }
+            }
+        }
+
+        // Récupère les paramètres spécifiés dans la route
         $params = explode('@', $r['script']);
         $controller = ucFirst($params[0]) . 'Controller';
 
@@ -18,8 +34,13 @@ foreach ($routes as $r) {
             $action = $params[1];
 
             if (method_exists($controller, $action)) {
+
+                // Appelle la fonction $action du contrôleur $controller
                 call_user_func_array([$controller, $action], []);
+                exit;
             }
         }
     }
 }
+
+ErrorController::page404($route);
