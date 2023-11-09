7e452f53d272dd46f2a7418565cbd31919d96c35
Mathéo Chaumet-Venier <matheo.chaumet.venier@gmail.com>
2023-11-09 20:48:07 +0100

    the indent issue

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -3,35 +3,36 @@
 class AuthController
 {
     public static function logIn()
-    {$token_gen = uniqid();
-     $admin = AdministrateurModel::getInstance()->findBy(['ad_mail_administrateur' => $_POST['email']]);
-     $joueur = JoueurModel::getInstance()->findBy(['ad_mail_joueur' => $_POST['email']]);
-     if ($admin) {
-         $user = $admin[0];
-         if ($_POST['password'] == $user['mot_de_passe_administrateur']) {
-             setcookie('email', $user['ad_mail_administrateur'], time() + (10 * 365 * 24 * 60 * 60));
-             setcookie('token', $token_gen, time() + time() + (10 * 365 * 24 * 60 * 60));
-             AdministrateurModel::getInstance()->update($user['id'], [
-                'token' => $token_gen,
-              ]);
-             HTTP::redirect('/admin');
-         }
-     } else {
-        LoginPage::render(['errors' => 'Identifiants invalides']);
-     }
-     if ($joueur) {
-         $user = $joueur[0];
-         if ($_POST['password'] == $user['mot_de_passe_joueur']) {
-            setcookie('email', $user['ad_mail_administrateur'], time() + (10 * 365 * 24 * 60 * 60));
-            setcookie('token', $token_gen, time() + time() + (10 * 365 * 24 * 60 * 60));
-             JoueurModel::getInstance()->update($user['id'], [
-                'token' => $token_gen,
-              ]); 
-            HTTP::redirect('/mon-espace');
-         }
-     } else {
-        LoginPage::render(['errors' => 'Identifiants invalides']);
-     }
+    {
+        $token_gen = uniqid();
+        $admin = AdministrateurModel::getInstance()->findBy(['ad_mail_administrateur' => $_POST['email']]);
+        $joueur = JoueurModel::getInstance()->findBy(['ad_mail_joueur' => $_POST['email']]);
+        if ($admin) {
+            $user = $admin[0];
+            if ($_POST['password'] == $user['mot_de_passe_administrateur']) {
+                setcookie('email', $user['ad_mail_administrateur'], time() + (10 * 365 * 24 * 60 * 60));
+                setcookie('token', $token_gen, time() + time() + (10 * 365 * 24 * 60 * 60));
+                AdministrateurModel::getInstance()->update($user['id'], [
+                    'token' => $token_gen,
+                ]);
+                HTTP::redirect('/admin');
+            }
+        } else {
+            LoginPage::render(['errors' => 'Identifiants invalides']);
+        }
+        if ($joueur) {
+            $user = $joueur[0];
+            if ($_POST['password'] == $user['mot_de_passe_joueur']) {
+                setcookie('email', $user['ad_mail_administrateur'], time() + (10 * 365 * 24 * 60 * 60));
+                setcookie('token', $token_gen, time() + time() + (10 * 365 * 24 * 60 * 60));
+                JoueurModel::getInstance()->update($user['id'], [
+                    'token' => $token_gen,
+                ]);
+                HTTP::redirect('/mon-espace');
+            }
+        } else {
+            LoginPage::render(['errors' => 'Identifiants invalides']);
+        }
     }
 
     public static function loginPage()
