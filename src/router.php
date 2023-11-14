0ac810eb82199f0e3591deeaa4c606399bce55c9
mchauveni <matheorigami@gmail.com>
2023-11-14 13:28:16 +0100

    added some security (not ideal) + indent issue

--- src/router.php
+++ src/router.php
@@ -10,16 +10,27 @@ $route = trim(str_replace(APP_ROOT_URL, '', $url['path']));
 
 $route = $route === '' ? '/' : $route;
 
-foreach ($routes as $r) {
-    if (in_array($route, $r['route']) && $_SERVER['REQUEST_METHOD'] === $r['method']) {
+foreach ($routes as $r)
+{
+    if (in_array($route, $r['route']) && $_SERVER['REQUEST_METHOD'] === $r['method'])
+    {
+
+        // Si y'a pas de token, redirect automatiquement sur le login
+        if (!$_COOKIE['token'])
+        {
+            HTTP::redirect('/login');
+        }
 
         // Si la route contient le mot "admin", on vérifie les droits
         // ! Ce code empêche uniquement  les non-admin d'accéder aux routes admin, pas l'inverse
-        if (in_array("admin", explode($route, "/"))) {
-            if (isset($_COOKIE['email']) && isset($_COOKIE['token'])) {
+        if (in_array("admin", explode($route, "/")))
+        {
+            if (isset($_COOKIE['email']) && isset($_COOKIE['token']))
+            {
                 $user = AdministrateurModel::getInstance()->findBy(['ad_mail_administrateur' => $_COOKIE['email']])[0];
 
-                if ($_COOKIE['token'] != $user['token']) {
+                if ($_COOKIE['token'] != $user['token'])
+                {
                     ErrorController::page403();
                     exit;
                 }
@@ -30,10 +41,12 @@ foreach ($routes as $r) {
         $params = explode('@', $r['script']);
         $controller = ucFirst($params[0]) . 'Controller';
 
-        if (class_exists($controller, true)) {
+        if (class_exists($controller, true))
+        {
             $action = $params[1];
 
-            if (method_exists($controller, $action)) {
+            if (method_exists($controller, $action))
+            {
 
                 // Appelle la fonction $action du contrôleur $controller
                 call_user_func_array([$controller, $action], []);
