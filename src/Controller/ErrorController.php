dc0a7cdaf71b2d34bdb36e76be92ca06cf2a9a75
mchauveni <matheorigami@gmail.com>
2023-11-14 13:27:59 +0100

    error controller should display 403 correctly now

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -1,18 +1,32 @@
 <?php
 
-class ErrorController {
+class ErrorController
+{
     /* Returns the view for page 404 */
-    public static function page404($route) {
-        if (isset($_COOKIE['token'])) {
+    public static function page404($route)
+    {
+        if (isset($_COOKIE['token']))
+        {
             $user_type = Utils::userType($_COOKIE['token']);
-        } else {
+        }
+        else
+        {
             $user_type = "";
         }
         Page404::render(["route" => $route, "user_type" => $user_type]);
     }
 
     /* Returns the view for page 403 */
-    public static function page403() {
-        Page403::render();
+    public static function page403()
+    {
+        if (isset($_COOKIE['token']))
+        {
+            $user_type = Utils::userType($_COOKIE['token']);
+        }
+        else
+        {
+            $user_type = "";
+        }
+        Page403::render(["route" => $route, "user_type" => $user_type]);
     }
 }
