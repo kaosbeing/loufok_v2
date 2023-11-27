576b0a393cc90cb25d6f936c1300b626c828042a
mchauveni <matheo.chaumet.venier@gmail.com>
2023-11-27 16:55:30 +0100

    API startup

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -1,3 +1,47 @@
 <?php
 
-class ApiController {};
+class ApiController {
+    public static function allLoufokeries() {
+        try {
+            $loufokeries = LoufokerieModel::getInstance()->findAll();
+            $data = [];
+            foreach ($loufokeries as $loufokerie) {
+                $contributions = ContributionModel::getInstance()->findByOrdered(["id_loufokerie" => $loufokerie["id"]]);
+                $loufokerie["contributions"] = $contributions;
+                array_push($data, $loufokerie);
+            }
+            $data = json_encode($data);
+            header('Content-Type: application/json');
+            echo $data;
+            exit;
+        } catch (PDOException $e) {
+            // Handle database connection errors
+            http_response_code(500);
+            echo 'Internal Server Error';
+            exit;
+        }
+    }
+
+    public static function loufokerie(int $id) {
+        try {
+            $loufokerie = LoufokerieModel::getInstance()->find($id);
+            $contributions = ContributionModel::getInstance()->findBy(["id_loufokerie" => $loufokerie["id"]]);
+
+            $data = [];
+
+            $loufokerie["joueurs"] = JoueurModel::getInstance()->GetAllNamesFromLoufokerie($id);
+            $loufokerie["contributions"] = $contributions;
+            array_push($data, $loufokerie);
+
+            $data = json_encode($data);
+            header('Content-Type: application/json');
+            echo $data;
+            exit;
+        } catch (PDOException $e) {
+            // Handle database connection errors
+            http_response_code(500);
+            echo 'Internal Server Error';
+            exit;
+        }
+    }
+};
--- src/Model/JoueurModel.php
+++ src/Model/JoueurModel.php
@@ -1,42 +1,64 @@
 <?php
 
 
-class JoueurModel extends Model
-{
+class JoueurModel extends Model {
     protected $tableName = APP_TABLE_PREFIX . 'joueur';
     protected $tableNameContribution = APP_TABLE_PREFIX . 'contribution';
     protected static $instance;
 
 
 
-    public static function getInstance()
-    {
-        if (!isset(self::$instance))
-        {
+    public static function getInstance() {
+        if (!isset(self::$instance)) {
             self::$instance = new self();
         }
 
         return self::$instance;
     }
-   /**
+    /**
      * Renvoie tout les joueurs par ordre alphabétique ayant participé a une loufokerie
      * @param int $id id de la loufokerie
      * @return array 
      * @return null
      */
-    public function findOrdered(int $id): ?array
-    {
+    public function findOrdered(int $id): ?array {
         $sql = "SELECT *
         FROM `{$this->tableName}` j
         JOIN `{$this->tableNameContribution}` c ON j.id = c.id_joueur
         WHERE c.id_loufokerie = :id
         ORDER BY j.nom_plume ASC";
         $sth = $this->query($sql, [':id' => $id]);
-        if ($sth && $sth->rowCount())
-        {
+        if ($sth && $sth->rowCount()) {
             return $sth->fetchAll();
         }
 
         return null;
+    }
+
+    /**
+     * Retrieves all names from all contribs from a Loufok
+     * 
+     * @param int $id id of the loufokerie
+     * @return array of all usernames, empty array if no usernames
+     */
+    public function GetAllNamesFromLoufokerie(int $id): array {
+        $usernames = [];
+        $contribs = ContributionModel::getInstance()->findBy(["id_loufokerie" => $id]);
+
+        if ($contribs) {
+            foreach ($contribs as $contrib) {
+                if ($contrib['id_joueur'] != null) {
+                    $users[] = JoueurModel::getInstance()->find($contrib["id_joueur"]);
+                }
+            }
+        }
+
+        if (isset($users)) {
+            foreach ($users as $user) {
+                $usernames[] = $user["nom_plume"];
+            }
+        }
+
+        return $usernames;
     }
 }
