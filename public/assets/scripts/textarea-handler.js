23e188f72b051f797e5dc55cdac5399cbba2ecdb
mchauveni <matheo.chaumet.venier@gmail.com>
2023-12-04 14:53:39 +0100

    some more random fixes

--- public/assets/scripts/textarea-handler.js
+++ public/assets/scripts/textarea-handler.js
@@ -1,46 +1,47 @@
 const form = document.querySelector(".form");
 const contrib = document.querySelector(".form__textarea");
 if (form) {
-  let nbCharac = contrib.value.length;
+	let nbCharac = contrib.value.length;
 
-  // Create a span element, position absolute into the form textarea
-  let lengthIndicator = document.createElement("span");
-  lengthIndicator.style.fontSize = "0.8rem";
-  lengthIndicator.style.position = "absolute";
-  lengthIndicator.style.right = "0.25rem";
-  lengthIndicator.style.textAlign = "right";
-  lengthIndicator.style.top = "50%";
-  lengthIndicator.style.transform = "translateY(-50%)";
+	// Create a span element, position absolute into the form textarea
+	let lengthIndicator = document.createElement("span");
+	lengthIndicator.style.fontSize = "0.8rem";
+	lengthIndicator.style.position = "absolute";
+	lengthIndicator.style.right = "0.25rem";
+	lengthIndicator.style.textAlign = "right";
+	lengthIndicator.style.top = "50%";
+	lengthIndicator.style.transform = "translateY(-50%)";
 
-  if (contrib != null) {
-    contrib.parentNode.appendChild(lengthIndicator); // Appends the string limit indicator to the textarea
-    // Both these function are called AFTER the draft is injected in textarea
-    // Autoresize is in settimeout because there was a bug where it was a not the right size on load
-    displayCharLimits();
-    setTimeout(() => {
-      autoResize();
-    }, 50);
+	if (contrib != null) {
+		contrib.parentNode.appendChild(lengthIndicator); // Appends the string limit indicator to the textarea
+		// Both these function are called AFTER the draft is injected in textarea
+		// Autoresize is in settimeout because there was a bug where it was a not the right size on load
+		displayCharLimits();
+		setTimeout(() => {
+			autoResize();
+		}, 50);
 
-    contrib.addEventListener("input", () => {
-      nbCharac = contrib.value.length;
+		contrib.addEventListener("input", () => {
+			nbCharac = contrib.value.length;
 
-      displayCharLimits();
-      autoResize();
-    });
-  }
+			displayCharLimits();
+			autoResize();
+		});
+	}
 
-  function autoResize() {
-    contrib.style.height = "auto"; // Reset the height to auto to determine the natural height
-    contrib.style.height = contrib.scrollHeight + "px"; // Set the height to match the content height
-  }
+	function autoResize() {
+		contrib.style.height = "auto"; // Reset the height to auto to determine the natural height
+		contrib.style.height = contrib.scrollHeight + "px"; // Set the height to match the content height
+	}
 
-function displayCharLimits() {
-  lengthIndicator.innerText = nbCharac;
+	function displayCharLimits() {
+		lengthIndicator.innerText = nbCharac;
 
-  if (nbCharac < 50 || nbCharac > 280) {
-    lengthIndicator.style.color = "red";
-  } else {
-    lengthIndicator.style.color = "green";
+		if (nbCharac < 50 || nbCharac > 280) {
+			lengthIndicator.style.color = "red";
+		} else {
+			lengthIndicator.style.color = "green";
 
-  }
+		}
+	}
 }
